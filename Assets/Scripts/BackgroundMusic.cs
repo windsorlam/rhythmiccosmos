efb8e1ed8aaa609f0b862c293f9ba276c45530d9
windsorlam <windsorlam1992@gmail.com>
2015-07-13 21:18:11 +0800

    bgm

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusic : MonoBehaviour {
+	public AudioSource music;
+	// Use this for initialization
+	void Start () {
+		if (!music.isPlaying) {
+			music.Play();
+		}
+	}
+}
---
+++ Assets/Scripts/GetMusic.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetMusic : MonoBehaviour {
+
+	void OnEnable () {
+		DataManager dm = DataManager.Instance;
+		string musicPath = dm.musicPath;
+		WWW www = new WWW (musicPath);
+		//yield return www;
+		audio.clip = www.audioClip;
+	}
+}

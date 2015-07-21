406aa50ad430cad861d4b923386a4d4930762cd0
Liu Xunyu <liuxunyu7@gmail.com>
2015-07-21 22:33:14 +0800

    1. Added multiplayer scene. 2. Started on the multi-player logic.

--- Assets/Scripts/Spwaner.cs
+++ Assets/Scripts/Spwaner.cs
@@ -1,14 +1,15 @@
-﻿//此脚本附加在Spwaner上，用来生成光晕
+//此脚本附加在Spwaner上，用来生成光晕
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class Spwaner :  MonoBehaviour
 {
 	public MainLogic logic;
+	public MultiMainLogic multiLogic;
+
     public GameObject elementPrefab;    // 光晕的预设体
     public Transform t;                 //光晕的克隆体预先设置的位置
-	public Queue<float> haloIntervalQueue = new Queue<float>();  //interval to create halo
+    public float interval;
 	public float speed;
 
 	int current;
@@ -16,20 +17,26 @@ public class Spwaner :  MonoBehaviour
     float timer = 0;
 	// Use this for initialization
 	void Start () {
-		DataManager dm=DataManager.Instance;
-		haloIntervalQueue = dm.onsetList;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		speed = logic.currentSpeed;
+		if(UIEvents.multiMode == true) {
+			speed = multiLogic.currentSpeed;
+		} else {
+			speed = logic.currentSpeed;
+		}
 
         timer += Time.deltaTime;
-        if (timer > haloIntervalQueue.Peek())
+        if (timer > interval)
         {
-			Debug.Log(timer);
             timer = 0;
-			current = logic.nextHighlight;
+			if(UIEvents.multiMode == true) {
+				current = multiLogic.nextHighlight;
+			} else {
+				current = logic.nextHighlight;
+			}
 
             GameObject g = Instantiate(elementPrefab) as GameObject;    //克隆一个光晕
             g.transform.position = t.position;          //光晕的位置与欧拉角和t一致
